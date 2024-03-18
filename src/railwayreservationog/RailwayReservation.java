bb47922eb8fce08bf14678953c90fe4d89b17734
ARSHAK MUHAMMED P K <quantumexploreram@yahoo.com>
2024-03-19 02:48:35 +0530

    Finished the project

---
+++ src/railwayreservationog/RailwayReservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.railwayreservation;
+
+/**
+ *
+ * @author arsha
+ */
+public class RailwayReservation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
